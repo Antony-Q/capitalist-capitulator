8712cd69e44e061f44f774314b9a0c4dc4b6ca01
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-02 15:13:33 -0500

    created employee table

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,13 @@
-DROP DATABASE IF EXISTS election;
-CREATE DATABASE election;
-USE election;
+DROP DATABASE IF EXISTS tracker;
+CREATE DATABASE tracker;
+USE tracker;
+
+CREATE TABLE employees(
+id INT AUTO_INCREMENT,
+first_name VARCHAR(25),
+last_name VARCHAR(25),
+job_title_id INT,
+manager_id INT,
+PRIMARY KEY(id),
+FOREIGN KEY(manager_id) REFERENCES employees (id)
+);
