0bc9faed2b54a4036d9cde2867d76a5ba21694c9
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-02 15:55:39 -0500

    updated db.sql

--- db/db.sql
+++ db/db.sql
@@ -11,3 +11,18 @@ manager_id INT,
 PRIMARY KEY(id),
 FOREIGN KEY(manager_id) REFERENCES employees (id)
 );
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT,
+    department_id VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE job_titl(
+    id INT AUTO_INCREMENT,
+    title VARCHAR(25),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
